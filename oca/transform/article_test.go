89f5d3d89ad84b7ffa00eff10e1a2eedd70eac94
Neo <99268950+n30w@users.noreply.github.com>
2025-01-01 20:25:46 -1000

    test: article tests

--- oca/transform/article_test.go
+++ oca/transform/article_test.go
@@ -43,7 +43,7 @@ func TestReadBackContent(t *testing.T) {
 			document: document{Type: "block"},
 			MarkDefs: []string{},
 			Style:    "normal",
-			Children: []portableTextChildNode{
+			Children: []contentBlock{
 				{
 					document: document{Type: "span"},
 					Marks:    []string{},
@@ -53,7 +53,7 @@ func TestReadBackContent(t *testing.T) {
 		},
 		{
 			document: document{Type: "block"},
-			Children: []portableTextChildNode{
+			Children: []contentBlock{
 				{
 					document: document{Type: "span"},
 					Marks:    []string{},
@@ -114,4 +114,128 @@ func TestReadBackContent(t *testing.T) {
 			t.Errorf("got: %s, want: %s", got[i].Style, want[i].Style)
 		}
 	}
+}
+
+func TestExtractImageFileName(t *testing.T) {
+	var pathTests = map[string]struct {
+		in  string
+		out string
+	}{
+		"i2.wp.com": {
+			"https://i2.wp.com/oncenturyavenue.org/wp-content/uploads/2020/09/微信图片_20200911213240.jpg?fit=640%2C502",
+			"oca-2020-09-微信图片_20200911213240.jpg",
+		},
+		"oncenturyavenue.org": {
+			"http://oncenturyavenue.org/wp-content/uploads/2018/02/media-20180214-3-1024x847.png",
+			"oca-2018-02-media-20180214-3.png",
+		},
+	}
+	for name, test := range pathTests {
+		t.Run(name, func(t *testing.T) {
+			// t.Parallel()
+			got, err := extractImageFileName(test.in)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			want := test.out
+
+			if got != want {
+				t.Fatalf("extractImageFilePath(%q) returned %q; expected %q", test.in, got, want)
+			}
+
+		})
+	}
+}
+
+func TestRetrieveImageRef(t *testing.T) {
+	m := mappings{
+		SanityImageFilename2SanityImageId: map[string]string{
+			"abc.png": "id-xyz",
+		},
+	}
+
+	f := retrieveImageRef(m)
+
+	want := "id-xyz"
+
+	got, err := f("abc.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got != want {
+		t.Fatalf("got %q, want %q", got, want)
+	}
+}
+
+func TestWalkContentBlocks(t *testing.T) {
+	t.Run("Ingest without edit", func(t *testing.T) {
+		want := "test_ref"
+
+		content := []contentBlock{
+			{
+				document: document{
+					Type: "image",
+				},
+				Asset: reference{Ref: want, Type: "reference"},
+			},
+		}
+
+		edits := []func(s string) (string, error){
+			func(s string) (string, error) { return s, nil },
+		}
+
+		c, err := walkContentBlocks("image", content, edits)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := c[0].Asset.Ref
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+
+	t.Run("Edits correctly", func(t *testing.T) {
+		want := "edited-correctly"
+
+		content := []contentBlock{
+			{
+				document: document{
+					Type: "image",
+				},
+				Asset: reference{Ref: "ref", Type: "reference"},
+			},
+		}
+
+		edits := []func(s string) (string, error){
+			func(s string) (string, error) {
+				return want, nil
+			},
+		}
+
+		content, err := walkContentBlocks("image", content, edits)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if content[0].Asset.Ref != want {
+			t.Errorf("got %s, want %s", content[0].Asset.Ref, want)
+		}
+	})
+}
+
+func TestNewArticle(t *testing.T) {
+	a := ocaArticle{}
+	m := mappings{}
+	c := config{}
+
+	article, err := newArticle(a, m, c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("%#v", article)
 }
